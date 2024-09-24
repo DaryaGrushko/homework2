62ff3ac8204483b1fafd75d2db7f479081b07961
DaryaGrushko <daryagrushko@gmail.com>
2024-09-24 21:08:33 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,6 @@
+#Практическое задание по теме "Переменные" 24.09
+count_homework=12
+count_hour=1.5
+name_cours='Python'
+time_on_one_class=count_hour/count_homework
+print('Курс: Python, всего задач: ', count_homework,', затрачено часов: ',count_hour,', среднее время выполнения ',time_on_one_class,' часа.')
